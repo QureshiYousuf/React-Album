ab1c92b090ad2726aeda9da77bfa59ca94f34bbb
Yusuf Qureshi <yousuf337692qureshi@gmail.com>
2024-01-29 07:22:04 +0530

    Final Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = (props) => {
   return (
     <div className='navbar'>
       <h2 onClick={() => props.handleClick(false)}>
-        <span className='brand-first-half'>ALBUMS</span>
+        <span className='brand-first-half'>ALBUM</span>
         <span className='brand-last-half'>LIST</span>
       </h2>
       <Link to={props.path}><button>{props.page}</button></Link>
